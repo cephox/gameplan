388c580de7973562bfa95beaa7420b3fa50e215f
cephox <paul-stier@gmx.de>
2020-12-07 15:34:47 +0100

    Changed icon

--- public/build_favicon.sh
+++ public/build_favicon.sh
@@ -1,4 +1,4 @@
 pushd .
 cd assets
-convert -resize x16 -gravity center -crop 16x16+0+0 icon.png -flatten -colors 256 -background transparent favicon.ico
+convert -resize x128 -gravity center -crop 128x128+0+0 icon.png -flatten -colors 256 -background transparent favicon.ico
 popd
